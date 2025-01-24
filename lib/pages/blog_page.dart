42057092c7e3a9a0c43cab03fd540b213f2d1621
newbe code <supriyd18@gmail.com>
2025-01-24 17:30:05 +0700

    update blog

--- lib/pages/blog_page.dart
+++ lib/pages/blog_page.dart
@@ -21,21 +21,54 @@ class BlogPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          SizedBox(height: 80,),
-
-          Row(
-            children: [
-              Column(
-                children: [
-                  Text("Artikel Terbaru"),
-                  Row(
-                    children: [
-                      CardBlog(),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+          SizedBox(
+            height: 80,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "Artikel Terbaru",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 150,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              CardBlog(
+                                image: 'https://i.pravatar.cc/100?u=',
+                                title: "Artikel 3",
+                                dateTime: "6/20/2025",
+                              ),
+                              CardBlog(
+                                image: 'https://i.pravatar.cc/100?u=',
+                                title: "Artikel 2",
+                                dateTime: "6/15/2025",
+                              ),
+                              CardBlog(
+                                image: 'https://i.pravatar.cc/100?u=',
+                                title: "Artikel 1",
+                                dateTime: "5/8/2025",
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
